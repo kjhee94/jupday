3a98a1fb29891d74dbb13183983051d28973f85c
kjh <kjhee_94@naver.com>
2021-12-23 15:38:36 +0900

    21년 12월 23일 오후 3시 38분 / 관리자페이지 js 추가

--- WebContent/assets/js/ui.js
+++ WebContent/assets/js/ui.js
@@ -106,9 +106,19 @@ $(document).ready(function(){
     
     
     //Admin 네비게이션 토글
-//    $(".mdl_nav > li").click(function() {
-//        $(this).next(".sub_nav").slideToggle();
-//
-//    });
+    $(".mdl_nav>li").click(function() {
+    	
+    	if($(this).next(".sub_nav").css("display")=="none"){
+    		$(this).next(".sub_nav").slideDown(250);
+    		$(this).find(".fa-chevron-down").css("transform","rotate(0deg)");
+    		
+    		$(".sub_nav").not($(this).next(".sub_nav")).slideUp(250);
+			$(".fa-chevron-down").not($(this).find(".fa-chevron-down")).css("transform","rotate(-90deg)");
+    	}else {
+    		$(this).next(".sub_nav").slideUp(250);
+    		$(this).find(".fa-chevron-down").css("transform","rotate(-90deg)");
+    	}
+
+    });
 });
 
