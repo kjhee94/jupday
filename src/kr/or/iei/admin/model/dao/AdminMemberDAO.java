7c35e357c0a1d74b7504d72764063420a413661f
kis <19x92@naver.com>
2021-12-25 04:39:31 +0900

    21년 12월 25일 오전 4시 40분 - 회원탈퇴 및 복원 로직 구현완료 - 김이서

--- src/kr/or/iei/admin/model/dao/AdminMemberDAO.java
+++ src/kr/or/iei/admin/model/dao/AdminMemberDAO.java
@@ -26,16 +26,17 @@ public class AdminMemberDAO {
 			//여러명이므로 while
 			while(rset.next()) 
 			{
-				Member m = new Member();
-				m.setAuthority_Id(rset.getString("authority_Id")); //1
-				m.setUserId(rset.getString("userId")); //2
-				m.setUserPwd(rset.getString("userPwd")); //3
-				m.setNick(rset.getString("nick")); //4
-				m.setEmail(rset.getString("email")); //5
-				m.setP_Image(rset.getString("p_Image")); //6
-				m.setEnrollDate(rset.getDate("enrollDate")); //7
-				m.setEnd_YN(rset.getString("end_yn").charAt(0)); //8
-				list.add(m);
+				//Member m은 MemberDAO와 겹쳐서 mem으로 대체
+				Member mem = new Member();
+				mem.setAuthority_Id(rset.getString("authority_Id")); //1
+				mem.setUserId(rset.getString("userId")); //2
+				mem.setUserPwd(rset.getString("userPwd")); //3
+				mem.setNick(rset.getString("nick")); //4
+				mem.setEmail(rset.getString("email")); //5
+				mem.setP_Image(rset.getString("p_Image")); //6
+				mem.setEnrollDate(rset.getDate("enrollDate")); //7
+				mem.setEnd_YN(rset.getString("end_yn").charAt(0)); //8
+				list.add(mem);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -54,4 +55,26 @@ public class AdminMemberDAO {
 		return query;
 	}
 
+	public int updateMemberEndYN(String userId, char endYN, Connection conn) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String query = "UPDATE MEMBER SET end_YN=? WHERE userId=?";
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			
+			pstmt.setString(1, String.valueOf(endYN));
+			pstmt.setString(2, userId);
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
 }
